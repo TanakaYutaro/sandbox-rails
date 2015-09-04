e84c4d79daae41303eed9e6d6b7ed1761450be43
yutaro <yutaro.tanaka.world@gmail.com>
2015-09-04 14:47:12 +0900

    Add picture model

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,36 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150902051917) do
+ActiveRecord::Schema.define(version: 20150904054612) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string   "title",      limit: 255
+    t.integer  "image_id",   limit: 4
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  add_index "articles", ["image_id"], name: "index_articles_on_image_id", using: :btree
 
   create_table "companies", force: :cascade do |t|
     t.string   "name",       limit: 255, null: false
     t.string   "type",       limit: 255
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
+  end
+
+  create_table "images", force: :cascade do |t|
+    t.string   "title",      limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  create_table "pictures", force: :cascade do |t|
+    t.string   "name",           limit: 255
+    t.integer  "imageable_id",   limit: 4
+    t.string   "imageable_type", limit: 255
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -30,4 +53,5 @@ ActiveRecord::Schema.define(version: 20150902051917) do
 
   add_index "users", ["company_id"], name: "index_users_on_company_id", using: :btree
 
+  add_foreign_key "articles", "images"
 end
